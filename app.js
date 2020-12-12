db8a2ef67ba4968b45d92fd4524f35a6c1f65af0
Milad.A <millad.99.awad@gmail.com>
2020-12-12 12:34:04 +0200

    Able to edit and update stories

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -25,6 +26,18 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    })
+)
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
@@ -36,6 +49,7 @@ const {
     stripTags,
     truncate,
     editIcon,
+    select,
 } = require('./helpers/hbs.js')
 
 // Handlebars
@@ -47,6 +61,7 @@ app.engine(
             stripTags,
             truncate,
             editIcon,
+            select,
         },
         defaultLayout: 'main',
         extname: '.hbs',
--- routes/stories.js
+++ routes/stories.js
@@ -41,4 +41,40 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc        Show edit page
+// @router      GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    const story = await Story.findOne({
+        _id: req.params.id,
+    }).lean()
+    if (!story) {
+        res.render('/error/404')
+    }
+    if (story.user != req.user.id) {
+        res.redirect('/stories')
+    } else {
+        res.render('stories/edit', {
+            story,
+        })
+    }
+})
+
+// @desc        update story
+// @router      PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    let story = await Story.findById(req.params.id).lean()
+    if (!story) {
+        return res.render('error/404')
+    }
+    if (story.user != req.user.id) {
+        res.redirect('/stories')
+    } else {
+        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        res.redirect('/dashboard')
+    }
+})
+
 module.exports = router
