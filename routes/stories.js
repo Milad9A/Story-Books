4d3acf4f42c9ae5880c2c2a0d179901a57540634
Milad.A <millad.99.awad@gmail.com>
2020-12-12 12:58:16 +0200

    Able to show single story

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,24 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc        Show single story
+// @router      GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).populate('user').lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+        res.render('stories/show', {
+            story,
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/404')
+    }
+})
+
 // @desc        Show edit page
 // @router      GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -95,6 +113,26 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     } catch (error) {
         console.error(error)
         return res.render('error/500')
+    }
+})
+
+// @desc        User stories
+// @router      GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public',
+        })
+            .populate('user')
+            .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
     }
 })
 
